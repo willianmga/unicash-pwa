bfa5691fa8071c1f1f74dd80ea9ecb5649654d29
Willian Azevedo <c-willian.azevedo@charter.net>
2018-09-23 00:35:16 -0300

    Added header

--- app/home-template.js
+++ app/home-template.js
@@ -2,14 +2,22 @@ class HomeTemplate {
 
     render() {
         document.querySelector("#content").innerHTML = `
-            <nav class="green darken-1">
+            <nav class="nav-extended green darken-1">
                 <div class="nav-wrapper">
-                <a href="#" class="brand-logo">Unicash</a>
+                    <a href="#" class="brand-logo">Unicash</a>
+                </div>
+                <div class="nav-content">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col s12">
+                                <h3 class="center-align">R$ 50,00</h3>
+                                <p class="center-align">Saldo</p>                            
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </nav>
             <div class="container">
-                <h3 class="center-align">R$ 50,00</h3>
-                <p class="center-align">Saldo</p>
                 <div class="row">
                     <div class="col s12">
                         <div class="card-panel">
