1fb819365a10d7736ba99be80994dca9b558f614
Lucas Sanches Fantacuci <lucasfantacuci@gmail.com>
2018-09-23 00:10:52 -0300

    home

--- app/home-template.js
+++ app/home-template.js
@@ -1,13 +1,35 @@
 class HomeTemplate {
+
     render() {
         document.querySelector("#content").innerHTML = `
-        <div class="card" style="width: 100%;">
-            <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <nav class="green darken-1">
+                <div class="nav-wrapper">
+                <a href="#" class="brand-logo">Unicash</a>
+                </div>
+            </nav>
+            <div class="container">
+                <h3 class="center-align">R$ 50,00</h3>
+                <p class="center-align">Saldo</p>
+                <div class="row">
+                    <div class="col s12">
+                        <div class="card-panel">
+                            <span>Multa bibliotecária</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col s12">
+                        <div class="card-panel">
+                            <span>Multa bibliotecária</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="fixed-action-btn">
+                    <a class="btn-floating btn-large green">
+                        <i class="large material-icons">add</i>
+                    </a>
+                </div>
             </div>
-        </div>
         `;
     }
 }
