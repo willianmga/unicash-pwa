9096bdf63970e64065b84936c2bd3211db4872b5
Lucas Sanches Fantacuci <lucasfantacuci@gmail.com>
2018-09-23 00:36:27 -0300

    footer

--- app/home-template.js
+++ app/home-template.js
@@ -7,29 +7,41 @@ class HomeTemplate {
                 <a href="#" class="brand-logo">Unicash</a>
                 </div>
             </nav>
-            <div class="container">
-                <h3 class="center-align">R$ 50,00</h3>
-                <p class="center-align">Saldo</p>
-                <div class="row">
-                    <div class="col s12">
-                        <div class="card-panel">
-                            <span>Multa bibliotecária</span>
+            <main>
+                <div class="container">
+                    <h3 class="center-align">R$ 50,00</h3>
+                    <p class="center-align">Saldo</p>
+                    <div class="row">
+                        <div class="col s12">
+                            <div class="card-panel">
+                                <span>Multa bibliotecária</span>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col s12">
-                        <div class="card-panel">
-                            <span>Multa bibliotecária</span>
+                    <div class="row">
+                        <div class="col s12">
+                            <div class="card-panel">
+                                <span>Multa bibliotecária</span>
+                            </div>
                         </div>
                     </div>
+                </div>  
+            </main>
+            <footer class="page-footer footer-fixed green">
+                <div class="row">
+                    <div class="col s3">a</div>
+                    <div class="col s3">b</div>
+                    <div class="col s3">c</div>
+                    <div class="col s3">d</div>
                 </div>
-                <div class="fixed-action-btn">
-                    <a class="btn-floating btn-large green">
-                        <i class="large material-icons">add</i>
-                    </a>
-                </div>
-            </div>
+            </footer>       
+            <style>
+            .page-footer.footer-fixed {
+                position: fixed;
+                bottom: 0;
+                width: 100%;
+            }
+            </style>   
         `;
     }
 }
